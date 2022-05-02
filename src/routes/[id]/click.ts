dd4f1116c0f5753e12cf917914a65be417f0bf65
ScalarZ <faresrahali44@gmail.com>
2022-05-02 17:53:32 +0100

    adding heroku server

--- src/routes/[id]/click.ts
+++ src/routes/[id]/click.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function get({ params }) {
-	const { data } = await axios.get('http://localhost:8080/user/' + params.id);
+	const { data } = await axios.get('https://poki-click.herokuapp.com/user/' + params.id);
 	if (data)
 		return {
 			body: {}
--- src/routes/[id]/collections/[charc].ts
+++ src/routes/[id]/collections/[charc].ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export async function get({ params }) {
-	const { data } = await axios.get('http://localhost:8080/charc/' + params.id + '/' + params.charc);
+	const { data } = await axios.get('https://poki-click.herokuapp.com/charc/' + params.id + '/' + params.charc);
 	const forms = data;
 	if (forms) {
 		return {
